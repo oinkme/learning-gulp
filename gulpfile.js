045f16f1f3a5aae8bd36f92b91cff486119ea3ff
Camilo Muñoz Donoso <camilo@universitame.com>
2015-04-28 15:06:06 -0300

    Added Autoprefixer + Imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	sass = require('gulp-ruby-sass'),
 	livereload = require('gulp-livereload'),
+	imagemin = require('gulp-imagemin'),
+	prefix = require('gulp-autoprefixer'),
 	connect = require('gulp-connect');
 	// Cuando hay errores, no detiene a Gulp
 	// plumber = require('gulp-plumber');
@@ -22,12 +24,14 @@ gulp.task('connect', function() {
   });
 });
 
+// Updates HTML
 gulp.task('html', function () {
   gulp.src('index.html')
     .pipe(connect.reload())
     .pipe(livereload());
 });
 
+// Scripts tasks, minifies js
 gulp.task('scripts', function() {
 	gulp.src('js/*.js')
 		.on('error', errorLog)
@@ -35,13 +39,23 @@ gulp.task('scripts', function() {
 		.pipe(gulp.dest('build/js'));
 });
 
-// Compiles Sass
+// Styles Task Compiles Sass
 gulp.task('styles', function() {
     return sass('scss/style.scss')
     	// Otra manera de manejar errores, sin pumbler
     	.on('error', errorLog)
+    	// Sirve para agregar autmaticamente prefixes al css
+    	// dependiendo la version de browser que se requiera
+    	.pipe(prefix('last 2 versions'))
     	.pipe(gulp.dest('css/'))
     	.pipe(livereload());
+});
+
+// Image Task, compress imgs
+gulp.task('images', function() {
+	gulp.src('img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('build/img'));
 });
 
 // Watch
@@ -50,10 +64,9 @@ gulp.task('watch', function() {
 	gulp.watch('scss/**/*.scss', ['styles']);
 	gulp.watch('js/*.js', ['scripts']);
 	gulp.watch('index.html', ['html']);
+	gulp.watch('img/*', ['images']);
 });
 
 
 // Default
 gulp.task('default', ['scripts', 'styles', 'watch', 'connect']);
-
-
