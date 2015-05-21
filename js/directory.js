286e66c04dd03b0ffc4fcbe109e387b34b97eb73
Camilo Muñoz Donoso <camilo@universitame.com>
2015-05-21 10:41:17 -0300

    Usando "Controller As Syntax"

--- js/directory.js
+++ js/directory.js
@@ -1,15 +1,18 @@
 angular.module('directoryApp', [])
-	.controller('directoryController', function($scope){
-		$scope.list = [
+	.controller('directoryController', function(){
+		
+		var dirList = this;
+
+		dirList.list = [
 			{name: 'Camilo', age: 27},
 			{name: 'Pamela', age: 24},
 			{name: 'Chris', age: 26},
 			{name: 'Caremono', age: 30}
 		];
 
-		$scope.addPerson = function() {
-			$scope.list.push({name:$scope.name, age:$scope.age});
-			$scope.name = '';
-			$scope.age = 0;
+		dirList.addPerson = function() {
+			dirList.list.push({name:dirList.name, age:dirList.age});
+			dirList.name = '';
+			dirList.age = 0;
 		};
 	});
